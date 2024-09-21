ba27ff1fd41b93d67caddb19d97148c280cb66da
Roshni Gaddam <43246927+Roshnigaddam@users.noreply.github.com>
2024-09-21 18:47:31 -0500

    Update grouped-anagrams.py

--- Blind75/arrays&hashing/grouped-anagrams.py
+++ Blind75/arrays&hashing/grouped-anagrams.py
@@ -1,6 +1,6 @@
-approach 1 : brute force: xxxxx - error - array index out of bounds:
+#approach 1 : brute force: xxxxx - error - array index out of bounds:
 
-code was:
+#code was:
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -35,5 +35,51 @@ class Solution:
 
 #error explaination : when I am removing elements from list, so when i am trying to append at index it is out of range
 
-#correction by adding flags for grouped elements instead of removing elements grouped
+#correction by adding flags for grouped elements instead of removing elements grouped --- xxx time out error
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        list1 = []
+        
+        grouped = [False]*len(strs)
+        
+        for i in range(len(strs)):
+            if(grouped[i]):
+                    continue
+            new_group = [strs[i]]
+            j=i+1
+            
+            grouped[i] = True
+            while(j < len(strs)):
+                
+                if(self.isAnagram(strs[i],strs[j])):
+                    new_group.append(strs[j])
+                    grouped[j]=True
+                    
+                j+=1
+            list1.append(new_group)
+        return list1
+
+    def isAnagram(self, str1: str, str2: str) -> bool:
+
+        # if str1 == "" and str2 == "":
+        #     return True
+        # # If only one is empty, they are not anagrams
+        # if len(str1) != len(str2):
+        #     return False
+        count = defaultdict(int)
+        for x in str1:
+            count[x]+=1
+        for x in str2:
+            count[x]-=1
+        
+        for val in count.values():
+            if val > 0:
+                return False
+
+        return True
+
+#case 4 for a very long array timeout error
+
 
