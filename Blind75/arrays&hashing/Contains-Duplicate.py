daaa15c467832699c10a862e6eb51462606b78af
Roshni Gaddam <43246927+Roshnigaddam@users.noreply.github.com>
2024-09-21 14:56:20 -0500

    Update Contains-Duplicate.py

--- Blind75/arrays&hashing/Contains-Duplicate.py
+++ Blind75/arrays&hashing/Contains-Duplicate.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+      
+      
+        count = defaultdict(int)
+        for x in nums:
+           count[x] += 1
+
+        for val in count.values():
+            if val > 1:
+                return True
+            
+        return False
