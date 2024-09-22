ebf9e1c4a042261ffe882f55d42d4e35b21d725d
Roshni Gaddam <43246927+Roshnigaddam@users.noreply.github.com>
2024-09-22 18:22:05 -0500

    Update valid-palindrome.py

--- Blind75/Two-pointer/valid-palindrome.py
+++ Blind75/Two-pointer/valid-palindrome.py
@@ -1,1 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        #slist=list(s)
+        cleaned_s = re.sub(r'[^a-zA-Z0-9]', '', s)
 
+        l=0
+        r=len(cleaned_s) - 1
+
+        while l < r:
+            print(cleaned_s[l],cleaned_s[r])
+            if cleaned_s[l]!=cleaned_s[r]:
+                return False
+            
+            l=l+1
+            r=r-1
+        return True
