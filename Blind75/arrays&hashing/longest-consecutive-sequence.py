392ef5680ef971b903ee1c390b5844648812fd57
Roshni Gaddam <43246927+Roshnigaddam@users.noreply.github.com>
2024-09-22 18:08:02 -0500

    Create longest-consecutive-sequence.py

---
+++ Blind75/arrays&hashing/longest-consecutive-sequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        res = []
+        num_set=set(nums)
+        longest_streak=0
+        for n in num_set:
+          if n-1 not in num_set:
+            current_element = n
+            current_streak=1
+            while current_element+1 in num_set:
+                   current_element +=1
+                   current_streak += 1
+                   #print(current_element)
+            longest_streak = max(longest_streak,current_streak)
+        return longest_streak
+
+        
