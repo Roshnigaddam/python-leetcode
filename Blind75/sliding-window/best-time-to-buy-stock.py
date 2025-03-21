7064cf6fde79ac00d78aecdefbcd7360c1be092a
Roshni Gaddam <43246927+Roshnigaddam@users.noreply.github.com>
2025-03-20 21:58:03 -0400

    Update Time Complexity

--- Blind75/sliding-window/best-time-to-buy-stock.py
+++ Blind75/sliding-window/best-time-to-buy-stock.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l,r=0,1
-        temp=0
-        maxp=0
 
-        while(r<len(prices) ):
-            print(l,r,prices[l],prices[r])
-            
-            if(prices[l] <= prices[r]):
-                temp = prices[r] - prices[l]
-                maxp=max(temp,maxp)
-                
-            else:
-                l=r
-            r+=1
-    
-        return(maxp)
+        min_price = float("inf")
 
-  ###keep updating left pointer to lowest price
+        max_profit =0
+
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            if max_profit < prices[i] - min_price:
+                max_profit = prices[i] - min_price
+        return max_profit
+
+
+
+# Time complexity: O(n). Only a single pass is needed.
+
+# Space complexity: O(1). Only two variables are used.
